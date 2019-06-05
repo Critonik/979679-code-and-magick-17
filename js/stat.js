8f4b6af74c9b096bdf44cf9a9e38653ec0cdc384
Critonik <42637808+Critonik@users.noreply.github.com>
2019-06-05 23:38:06 +0500

    линтер

--- js/stat.js
+++ js/stat.js
@@ -11,15 +11,14 @@ var TEXT_WIDTH = 50;
 var BAR_WIDTH = 40;
 var barHeight = CLOUD_HEIGHT - GAP - TEXT_WIDTH - FONT_GAP;
 
-
-var renderCloud = function(ctx, x, y, color) {
+var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
-var getMaxElement = function(arr) {
-  var maxElement = arr[0];
+var getMaxElement = function (arr) {
 
+  var maxElement = arr[0];
   for (var i = 0; i < arr.length; i++) {
     if (arr[i] > maxElement) {
       maxElement = arr[i];
@@ -29,11 +28,10 @@ var getMaxElement = function(arr) {
   return maxElement;
 };
 
-window.renderStatistics = function(ctx, names, times) {
+window.renderStatistics = function (ctx, names, times) {
+
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
-
-
   ctx.font = '14px PT Mono';
   ctx.fillStyle = '#000';
   ctx.textBaseline = 'hanging';
@@ -45,5 +43,5 @@ window.renderStatistics = function(ctx, names, times) {
   for (var i = 0; i < names.length; i++) {
     ctx.fillText(names[i], CLOUD_X + TEXT_WIDTH + GAP + (TEXT_WIDTH + BAR_WIDTH) * i, CLOUD_HEIGHT - DOUBLE_GAP);
     ctx.fillRect(CLOUD_X + TEXT_WIDTH + (TEXT_WIDTH + BAR_WIDTH) * i, CLOUD_X - DOUBLE_GAP, BAR_WIDTH, ((barHeight * times[i]) / maxTime));
-  };
+  }
 };
