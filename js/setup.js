32159edde77b8a43739d05c56a920ee3938510a7
Critonik <42637808+Critonik@users.noreply.github.com>
2019-06-09 12:26:10 +0500

    исправил по замечаниям

--- js/setup.js
+++ js/setup.js
@@ -4,7 +4,7 @@ document.querySelector('.setup-similar').classList.remove('hidden');
 var similarListElement = document.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
 var firstName = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Пупа', 'Топольницкая', 'Вашингтон'];
-var secondName = [' да Марья', ' Верон', ' Мирабелла', ' Вальц', ' Онопко', ' Лупа', ' Нионго', ' Ирвинг'];
+var secondName = [' да Марья', ' Верон', ' Мирабелла', ' Вальц', ' Онопко', ' и Лупа', ' Нионго', ' Ирвинг'];
 var color = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var eyeColor = ['red', 'black', 'blue', 'yellow', 'green'];
 
@@ -13,19 +13,15 @@ function getRandomInt(min, max) {
 }
 
 var createName = function (name, surName) {
-  name = name[getRandomInt(0, name.length)];
-  surName = surName[getRandomInt(0, surName.length)];
-  var createdName = name + surName;
-  return createdName;
+  return name[getRandomInt(0, name.length)] + '' + surName[getRandomInt(0, name.length)];
 };
-
-var name = createName(firstName, secondName);
 
 var wizards = [];
 
 for (var i = 0; i < 4; i++) {
-  wizards.join = ({
-    name: name[i],
+  var name = createName(firstName, secondName);
+  wizards.push ({
+    name: name,
     coatColor: color[getRandomInt(0, color.length)],
     eyesColor: eyeColor[getRandomInt(0, eyeColor.length)]
   });
